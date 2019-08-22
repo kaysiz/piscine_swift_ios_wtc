a4006981b4086c8c38793b144b71389ca264147f
Kudakwashe Siziva <ksiziva@gmail.com>
2019-08-22 13:38:13 +0200

    day00 ex00

---
+++ day00/ex00/ex00/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by kudakwashe on 2019/08/21.
+//  Copyright © 2019 WeThinkCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func clickMe() {
+         print("I have been clicked, yeeeey!!")
+    }
+}
+
