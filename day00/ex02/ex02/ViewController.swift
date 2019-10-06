3d8bcfe5cbef53bb486cd440fee816bce41aa376
Kudakwashe SIZIVA <ksiziva@c5r7s8.wethinkcode.co.za>
2019-10-06 16:11:38 +0200

    Added button funcs

--- day00/ex02/ex02/ViewController.swift
+++ day00/ex02/ex02/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var laNumberShow: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func AddNumberToInput(number:String){
+        var textNumber = String(laNumberShow.text!)
+        textNumber = textNumber + number
+        laNumberShow.text = textNumber
+    }
+    
+    @IBAction func bu0(_ sender: Any) {
+        AddNumberToInput(number: "0")
+    }
+    @IBAction func bu1(_ sender: Any) {
+        AddNumberToInput(number: "1")
+    }
+    @IBAction func bu2(_ sender: Any) {
+        AddNumberToInput(number: "2")
+    }
+    @IBAction func bu3(_ sender: Any) {
+        AddNumberToInput(number: "3")
+    }
+    @IBAction func bu4(_ sender: Any) {
+        AddNumberToInput(number: "4")
+    }
+    @IBAction func bu5(_ sender: Any) {
+        AddNumberToInput(number: "5")
+    }
+    @IBAction func bu6(_ sender: Any) {
+        AddNumberToInput(number: "6")
+    }
+    @IBAction func bu7(_ sender: Any) {
+        AddNumberToInput(number: "7")
+    }
+    @IBAction func bu8(_ sender: Any) {
+        AddNumberToInput(number: "8")
+    }
+    @IBAction func bu9(_ sender: Any) {
+        AddNumberToInput(number: "9")
+    }
+    
 }
 
