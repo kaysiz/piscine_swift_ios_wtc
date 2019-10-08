bdcb97307ed0db4a5b7cdf5df6742a71452fd260
Kudakwashe Siziva <ksiziva@gmail.com>
2019-10-08 06:21:29 +0200

    Day02 work in progress

---
+++ day02/day02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day02
+//
+//  Created by kudakwashe on 2019/10/07.
+//  Copyright © 2019 WeThinkCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
